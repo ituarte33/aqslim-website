3acbdf682965321f34b14c2e93343506feba98ee
Romit1966 <aqslim@gmail.com>
2026-09-11 17:59:35 -0700

    Expose Founder D10 third-correction probe on saved scan editor

--- app/food-scanner/edit/[mealLogId]/page.tsx
+++ app/food-scanner/edit/[mealLogId]/page.tsx
@@ -1,4 +1,5 @@
 import { SavedScanEditor } from './saved-scan-editor'
+import { ReanalysisLimitProbe } from './reanalysis-limit-probe'
 
 export const dynamic = 'force-dynamic'
 
@@ -8,5 +9,12 @@ export default async function SavedScanEditPage({
   params: Promise<{ mealLogId: string }>
 }) {
   const { mealLogId } = await params
-  return <SavedScanEditor mealLogId={mealLogId} />
+  return (
+    <>
+      <SavedScanEditor mealLogId={mealLogId} />
+      <div style={{ maxWidth: 900, margin: '0 auto', padding: '0 20px 64px', background: '#0b0b0b', color: '#eee' }}>
+        <ReanalysisLimitProbe mealLogId={mealLogId} />
+      </div>
+    </>
+  )
 }
